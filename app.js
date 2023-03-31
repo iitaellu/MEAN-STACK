3475cc975279f819bed74893bc3a716a2168324a
me mee <me@email.com>
2023-04-01 02:21:16 +0300

    protection of pages

--- app.js
+++ app.js
@@ -58,6 +58,10 @@ app.get("/", (req, res) => {
     res.send('Invalid Endpoint')
 })
 
+app.get('*'), (req,res) => {
+    res.sendFile(path.join(__dirname, 'piblic/index.html'));
+}
+
 // Start Server
 app.listen(port, () => {
     console.log("Server started on port " + port);
