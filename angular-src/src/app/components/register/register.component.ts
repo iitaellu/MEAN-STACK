54c53cd711a75a73b42be26acd9b00f31d378f3d
me mee <me@email.com>
2023-03-31 18:03:56 +0300

    login and logout

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -37,25 +37,25 @@ export class RegisterComponent implements OnInit {
 
       this.flashMessage.show('Please fill in all fields',{cssClass: 'alert-danger', timeout: 3000});
       return false;
-  }
-  // Required Email
+    }
+    // Required Email
     if(!this.validateService.validateEmail(user.email)){
 
     this.flashMessage.show('Please use a valid email',{cssClass: 'alert-danger', timeout: 3000});
     return false;
-  }
+    }
 
-  // REgister User
-  this.authService.registerUser(user).subscribe(data =>{
+     // REgister User
+    this.authService.registerUser(user).subscribe(data =>{
     if(data.success){
       this.flashMessage.show('You are now registered and can log in',{cssClass: 'alert-success', timeout: 3000});
       this.router.navigate(['/login'])
-    } else {
+      } else {
       this.flashMessage.show('Something went wrongn',{cssClass: 'alert-danger', timeout: 3000});
       this.router.navigate(['/register'])
-    }
-  })
+      }
+    })
 
-}
+  }
 
 }
