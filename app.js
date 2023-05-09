fbd310969028afd06b429b01c2f9ae41bf4f0041
me mee <me@email.com>
2023-05-09 17:12:48 +0300

    fixed mayby

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/", (req, res) => {
 })
 
 app.get('*'), (req,res) => {
-    res.sendFile(path.join(__dirname, 'piblic/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 }
 
 // Start Server
